3579aef0bc3d3540b943786473845f8b30ec36f9
geekfreak <56112622+GeekFreak01@users.noreply.github.com>
2025-06-20 10:29:05 +0500

    Create key_handler.py

---
+++ key_handler.py
@@ -0,0 +1,19 @@
+# key_handler.py
+import keyboard
+import time
+
+def setup_keybindings(obs_client):
+    # Примеры назначения кнопок
+    keyboard.add_hotkey('f1', lambda: obs_client.set_scene('Scene 1'))
+    keyboard.add_hotkey('f2', lambda: obs_client.set_scene('Scene 2'))
+    keyboard.add_hotkey('f3', lambda: obs_client.toggle_mic())
+    keyboard.add_hotkey('f4', lambda: obs_client.start_recording())
+    keyboard.add_hotkey('f5', lambda: obs_client.stop_recording())
+
+    print("⌨️ Hotkeys are active (press ESC to stop)...")
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print("🛑 Stopping hotkey handler...")
+            break
